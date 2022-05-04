8e630f73c2e1c744c4590057856973382ca6c6e1
Yasikhorram <khorramyasaman@gmail.com>
2022-05-03 22:30:26 -0500

    add direction rtl to total section

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.querySelector(".row7").addEventListener("click", calc);
+
+function calc() {
+  let total = 0;
+  const num1 = document.querySelector(".row7");
+
+  document.querySelector("#total").innerText = "You got this";
+}
